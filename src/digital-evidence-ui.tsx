a06d810454a8c70e8b3cc2a921a838d438eaabc7
Walter Cerna <walter@cernacreations.com>
2025-09-03 12:32:07 -0400

    update activity tab label

--- src/digital-evidence-ui.tsx
+++ src/digital-evidence-ui.tsx
@@ -40,7 +40,7 @@ const DigitalEvidenceUI = () => {
             className={`px-4 py-2 rounded-md font-medium focus:outline-none ${activeTab === 'analysis' ? 'bg-blue-600 text-white' : 'bg-white text-blue-600 border border-blue-600'}`}
             onClick={() => setActiveTab('analysis')}
           >
-            <Search className="inline-block h-5 w-5 mr-2" /> Analysis
+            <Search className="inline-block h-5 w-5 mr-2" /> Activity
           </button>
         </div>
         <div>
